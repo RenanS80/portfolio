ace50233aa42ce26cf5a4cd42285d94ac44f52e5
Renan Soares <renansoaresffc@gmail.com>
2022-03-19 16:12:04 -0300

    New project controls, projects description and buttons, custom scroll

--- assets/js/changeSlide.js
+++ assets/js/changeSlide.js
@@ -1,24 +1,19 @@
 export function changeSlide() {
 
     let slides = document.querySelectorAll('.project .slide-container .slide')
-    let radio = document.querySelectorAll('.project .slide-container .radio-slide')
     let index = 0;
     const prevSlide = document.getElementById('prev-slide');
     const nextSlide = document.getElementById('next-slide');
 
     prevSlide.addEventListener('click', () => {
         slides[index].classList.remove('active')
-        radio[index].removeAttribute('checked')
         index = (index - 1 + slides.length) % slides.length;
         slides[index].classList.add('active')
-        radio[index].setAttribute('checked', 'checked')
     });
 
     nextSlide.addEventListener('click', () => {
         slides[index].classList.remove('active')
-        radio[index].removeAttribute('checked')
         index = (index + 1) % slides.length;
         slides[index].classList.add('active')
-        radio[index].setAttribute('checked', 'checked')
     });
 }
